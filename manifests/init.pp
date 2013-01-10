7db1ff5a3ce6ab3f7d2b2bf474e6e0ee1bf41a80
Robert Starmer <starmer@cisco.com>
2013-01-10 13:49:19 -0800

    Class and define work

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,9 @@
-define me ($user_name='starmer', $ensure='present') {
+class me ($user_name='starmer', $ensure='present') {
+ user {"${user_name}":
+   ensure => "${ensure}",
+ }
+}
+define me::use_me ($user_name='starmer', $ensure='present') {
  user {"${user_name}":
    ensure => "${ensure}",
  }
